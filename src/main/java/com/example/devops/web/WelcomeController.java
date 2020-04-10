e4c9b024ecf478c4d02f85df0745caef372e9dae
srimn1 <srimn1@yahoo.co.in>
2020-04-10 10:46:03 +0530

    dfd

--- src/main/java/com/example/devops/web/WelcomeController.java
+++ src/main/java/com/example/devops/web/WelcomeController.java
@@ -1,12 +1,17 @@
 package com.example.devops.web;
 
+import org.apache.logging.log4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 public class WelcomeController {
 
+	private final Logger logger = (Logger) LoggerFactory.getLogger(this.getClass());
+
 	@RequestMapping()
 	public String welcome(Model model) {
+		logger.info("Processing index request");
 		model.addAttribute("course", "DevOps");
 		return "index";
 	}
